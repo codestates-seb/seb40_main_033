affef354784d2eac72238216fa035ed84f177da2
Jihyo <107678471+zirryo@users.noreply.github.com>
2022-11-14 10:53:14 +0900

    Add files via upload

---
+++ server/src/main/java/server/team33/Team33Application.java
@@ -0,0 +1,13 @@
+package server.team33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team33Application.class, args);
+	}
+
+}
