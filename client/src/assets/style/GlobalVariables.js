3268771bb84f809eb5d72e07cd1e9f944c24d77a
kihyeoon <bangion93@gmail.com>
2022-11-16 20:05:53 +0900

    :lipstick: design: 글로벌스타일 설정 #14

---
+++ client/src/assets/style/GlobalVariables.js
@@ -0,0 +1,19 @@
+import { css } from 'styled-components';
+
+const variables = css`
+	--purple-100: #f8f7ff;
+	--purple-200: #6255f6;
+	--purple-300: #6256e5;
+	--gray-100: #fafafb;
+	--gray-200: #d9d9d9;
+	--gray-300: #acaebb;
+	--gray-400: #838383;
+	--gray-500: #414141;
+	--gray-600: #181818;
+	--green-100: #74eb8b;
+	--green-200: #00c125;
+	--orange-100: #fe9701;
+	--red-100: #dd3f3f;
+`;
+
+export default variables;
