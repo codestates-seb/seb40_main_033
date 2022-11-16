7333646a6ec87bd66454523417c70f3c10009bda
Jihyo <jh04187@gmail.com>
2022-11-16 13:19:18 +0900

    :sparkles: feat: Cart 관련 ExceptionCode 추가 #3

--- server/src/main/java/server/team33/exception/bussiness/ExceptionCode.java
+++ server/src/main/java/server/team33/exception/bussiness/ExceptionCode.java
@@ -6,7 +6,9 @@ public enum ExceptionCode {
 
     EXIST_EMAIL(409,"이미 가입한 e-mail입니다."),
     EXIST_DISPLAY_NAME(409,"이미 존재하는 닉네임입니다."),
-    EXIST_PHONE_NUMBER(409,"이미 존재하는 연락처입니다.");
+    EXIST_PHONE_NUMBER(409,"이미 존재하는 연락처입니다."),
+    CART_NOT_FOUND(404, "존재하지 않는 카트입니다."),
+    ITEMCART_NOT_FOUND(404, "존재하지 않는 항목입니다.");
 
     @Getter
     private int code;
