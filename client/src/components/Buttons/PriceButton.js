f16a28158aa8e90e76a889b6f91759a7d05ca873
dohyeons <ddhhss0603@gmail.com>
2022-11-22 01:45:08 +0900

    :lipstick: design: 가격정렬 버튼 모음 컴포넌트 UI 구현 #19

--- client/src/components/Buttons/PriceButton.js
+++ client/src/components/Buttons/PriceButton.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
+import { BsDash } from 'react-icons/bs';
 
 export default function MultiRangeSlider({ min, max }) {
 	const [minVal, setMinVal] = useState(min);
@@ -38,47 +39,67 @@ export default function MultiRangeSlider({ min, max }) {
 	// Get min and max values when their state changes
 
 	return (
-		<SliderContainer>
-			<ThumbLeft
-				type="range"
-				min={min}
-				max={max}
-				value={minVal}
-				step="1000"
-				onChange={(event) => {
-					const value = Math.min(Number(event.target.value), maxVal - 1000);
-					setMinVal(value);
-					minValRef.current = value;
-				}}
-				className="thumb"
-				// style={{ zIndex: minVal > max - 100 && '5' }}
-			/>
-			<ThumbRight
-				type="range"
-				min={min}
-				max={max}
-				step="1000"
-				value={maxVal}
-				onChange={(event) => {
-					const value = Math.max(Number(event.target.value), minVal + 1000);
-					setMaxVal(value);
-					maxValRef.current = value;
-				}}
-				className="thumb"
-			/>
+		<EntireContainer>
+			<SliderContainer>
+				<ThumbLeft
+					type="range"
+					min={min}
+					max={max}
+					value={minVal}
+					step="1000"
+					onChange={(event) => {
+						const value = Math.min(Number(event.target.value), maxVal - 1000);
+						setMinVal(value);
+						minValRef.current = value;
+					}}
+					className="thumb"
+				/>
+				<ThumbRight
+					type="range"
+					min={min}
+					max={max}
+					step="1000"
+					value={maxVal}
+					onChange={(event) => {
+						const value = Math.max(Number(event.target.value), minVal + 1000);
+						setMaxVal(value);
+						maxValRef.current = value;
+					}}
+					className="thumb"
+				/>
 
-			<Slider>
-				<SliderTrack />
-				<SliderRange ref={range} />
-				<SliderLeftValue>{minVal}원</SliderLeftValue>
-				<SliderRightValue>{maxVal}원</SliderRightValue>
-			</Slider>
-		</SliderContainer>
+				<Slider>
+					<SliderTrack />
+					<SliderRange ref={range} />
+				</Slider>
+			</SliderContainer>
+			<ButtomContainer>
+				<ValueBox>
+					<ValueDescription>최저 금액</ValueDescription>
+					<SliderValue>{minVal} 원</SliderValue>
+				</ValueBox>
+				<IconBox>
+					<BsDash />
+				</IconBox>
+				<ValueBox>
+					<ValueDescription>최고 금액</ValueDescription>
+					<SliderValue>{maxVal} 원</SliderValue>
+				</ValueBox>
+				<AdoptButton>적용하기</AdoptButton>
+			</ButtomContainer>
+		</EntireContainer>
 	);
 }
 
+const EntireContainer = styled.div`
+	display: inline-flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid;
+`;
+
 const SliderContainer = styled.div`
-	height: 100vh;
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -91,43 +112,13 @@ const SliderContainer = styled.div`
 	}
 `;
 
-const Slider = styled.div`
-	position: relative;
-	width: 360px;
-	div {
-		position: absolute;
-	}
-`;
-
-const SliderTrack = styled.div`
-	background-color: var(--gray-300);
-	width: 100%;
-	z-index: 1;
-	height: 2px;
-`;
-const SliderRange = styled.div`
-	background-color: var(--purple-200);
-	z-index: 2;
-	height: 2px;
-`;
-const SliderLeftValue = styled.div`
-	color: var(--gray-400);
-	font-size: 13px;
-	margin-top: 20px; //위의 3개는 보여주는 값의 설정입니다. 밑의 RightValue에도 똑같은 설정 있습니다.
-`;
-const SliderRightValue = styled.div`
-	color: var(--gray-400);
-	font-size: 13px;
-	margin-top: 20px;
-`;
-
 const ThumbLeft = styled.input`
 	-webkit-appearance: none;
 	&::-webkit-slider-thumb {
 		-webkit-appearance: none;
 		-webkit-tap-highlight-color: transparent;
 		background-color: white;
-		border: 1px solid #6255f6;
+		border: 1px solid var(--purple-200);
 		border-radius: 50%;
 		box-shadow: 0 0 1px 1px #ced4da;
 		cursor: pointer;
@@ -145,7 +136,7 @@ const ThumbRight = styled.input`
 		-webkit-appearance: none;
 		-webkit-tap-highlight-color: transparent;
 		background-color: white;
-		border: 1px solid #6255f6;
+		border: 1px solid var(--purple-200);
 		border-radius: 50%;
 		box-shadow: 0 0 1px 1px #ced4da;
 		cursor: pointer;
@@ -156,4 +147,82 @@ const ThumbRight = styled.input`
 		position: relative;
 	}
 	z-index: 4;
+`;
+
+const Slider = styled.div`
+	position: relative;
+	width: 360px;
+`;
+
+const SliderTrack = styled.div`
+	background-color: var(--gray-300);
+	position: absolute;
+	width: 100%;
+	z-index: 1;
+	height: 2px;
+`;
+const SliderRange = styled.div`
+	background-color: var(--purple-200);
+	position: absolute;
+	z-index: 2;
+	height: 2px;
+`;
+
+const ButtomContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	margin-top: 34px;
+`;
+
+const ValueBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 106px;
+	height: 42px;
+	border: 0.8px solid var(--gray-300);
+	border-radius: 6px;
+	padding: 7px;
+`;
+const ValueDescription = styled.div`
+	color: var(--gray-300);
+	font-size: 9px;
+	margin-bottom: 6px;
+`;
+
+const SliderValue = styled.div`
+	color: var(--gray-400);
+	font-size: 13px;
+	//위의 3개는 보여주는 값의 설정입니다. 밑의 RightValue에도 똑같은 설정 있습니다.
+`;
+
+const IconBox = styled.div`
+	margin: 0px 23px;
+	& > svg {
+		path {
+			color: var(--gray-300);
+		}
+	}
+`;
+
+const AdoptButton = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 69px;
+	height: 32px;
+	border: 0.5px solid var(--purple-200);
+	border-radius: 6px;
+	background-color: var(--purple-100);
+	font-size: 11px;
+	font-weight: var(--bold);
+	color: var(--purple-200);
+	transition: 0.3s all;
+	cursor: pointer;
+	margin-left: 20px;
+	&:hover {
+		background-color: var(--purple-200);
+		color: var(--purple-100);
+	}
 `;
