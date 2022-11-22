f5007ffeb79075a2b9b35df8a4fb88c35bde9306
uxolrv <uxolrv@gmail.com>
2022-11-22 18:05:02 +0900

    :sparkles: feat: 라우터 네스팅 작업 #66

--- client/src/components/Layout/RightNav.js
+++ client/src/components/Layout/RightNav.js
@@ -5,6 +5,7 @@ import {
 	AiOutlineSearch,
 	AiOutlineShoppingCart,
 } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 import { turn, listHover } from './LeftNav';
 
 function RightNav() {
@@ -25,6 +26,71 @@ function RightNav() {
 				/>
 				<AiOutlineShoppingCart />
 			</IconContainer>
+			<RouteContainer>
+				<Member>
+					기현
+					<button type="button">
+						<Link to="signup">회원가입</Link>
+					</button>
+					<button type="button">
+						<Link to="login">로그인</Link>
+					</button>
+					<button type="button">
+						<Link to="/">메인</Link>
+					</button>
+					<button type="button">
+						<Link to="mypage/note/review">리뷰관리</Link>
+					</button>
+					<button type="button">
+						<Link to="mypage/note/talk">토크관리</Link>
+					</button>
+				</Member>
+				<Member>
+					현수
+					<button type="button">
+						<Link to="mypage/user-info">회원정보</Link>
+					</button>
+					<button type="button">
+						<Link to="mypage/sub-manage">정기구독목록</Link>
+					</button>
+					<button type="button">
+						<Link to="mypage/wish">위시리스트</Link>
+					</button>
+					<button type="button">
+						<Link to="pay/normal">일반결제</Link>
+					</button>
+					<button type="button">
+						<Link to="pay/subscription">정기결제</Link>
+					</button>
+				</Member>
+				<Member>
+					세연
+					<button type="button">
+						<Link to="mypage/order">주문내역</Link>
+					</button>
+					<button type="button">
+						<Link to="mypage/order/1">주문내역상세</Link>
+					</button>
+					<button type="button">
+						<Link to="detail/1">상세</Link>
+					</button>
+				</Member>
+				<Member>
+					지환
+					<button type="button">
+						<Link to="cart/normal">일반장바구니</Link>
+					</button>
+					<button type="button">
+						<Link to="cart/subscription">정기장바구니</Link>
+					</button>
+					<button type="button">
+						<Link to="list">목록</Link>
+					</button>
+					<button type="button">
+						<Link to="search">검색목록</Link>
+					</button>
+				</Member>
+			</RouteContainer>
 		</Nav>
 	);
 }
@@ -58,37 +124,37 @@ const showSearchBar = keyframes`
 // 	position: relative;
 // `;
 
-const Box = styled.div`
-	/* margin-right: 230px; */
-	/* width: 248px; */
-	/* height: 47.5px; */
+// const Box = styled.div`
+// 	/* margin-right: 230px; */
+// 	/* width: 248px; */
+// 	/* height: 47.5px; */
 
-	margin: 7px 0;
-	display: flex;
-	/* justify-content: flex-end; */
-	align-items: center;
-	/* stroke-width: 10;
-	color: var(--green-100);
-	font-size: 24px; */
-	position: absolute;
-	/* right: 270px; */
-	/* top: 54px; */
-	/* right: 5px; */
-	svg {
-		display: flex;
-		align-items: center;
-		position: absolute;
-		border: none;
-		background: none;
-		stroke-width: 10;
-		color: var(--green-100);
-		font-size: 24px;
-		cursor: pointer;
-		/* z-index: 4; */
-		left: 260px;
-		z-index: 999;
-	}
-`;
+// 	margin: 7px 0;
+// 	display: flex;
+// 	/* justify-content: flex-end; */
+// 	align-items: center;
+// 	/* stroke-width: 10;
+// 	color: var(--green-100);
+// 	font-size: 24px; */
+// 	position: absolute;
+// 	/* right: 270px; */
+// 	/* top: 54px; */
+// 	/* right: 5px; */
+// 	svg {
+// 		display: flex;
+// 		align-items: center;
+// 		position: absolute;
+// 		border: none;
+// 		background: none;
+// 		stroke-width: 10;
+// 		color: var(--green-100);
+// 		font-size: 24px;
+// 		cursor: pointer;
+// 		/* z-index: 4; */
+// 		left: 260px;
+// 		z-index: 999;
+// 	}
+// `;
 
 const Input = styled.input`
 	display: flex;
@@ -109,25 +175,10 @@ const Input = styled.input`
 	}
 	::placeholder {
 		color: var(--gray-300);
-		/* text-align: center; */
 		text-indent: 24px;
 		text-rendering: 24px;
 	}
 `;
-
-// const Icon = styled.button`
-// 	display: flex;
-// 	align-items: center;
-// 	position: absolute;
-// 	border: none;
-// 	background: none;
-// 	stroke-width: 10;
-// 	color: var(--green-100);
-// 	font-size: 24px;
-// 	cursor: pointer;
-// 	/* z-index: 4; */
-// 	left: 240px;
-// `;
 
 const IconContainer = styled.li`
 	display: flex;
@@ -135,7 +186,6 @@ const IconContainer = styled.li`
 	align-items: flex-end;
 	-webkit-user-select: none;
 	user-select: none;
-	cursor: pointer;
 
 	& > svg {
 		margin: 15px 0;
@@ -156,6 +206,7 @@ const IconContainer = styled.li`
 	}
 
 	path {
+		/* cursor: pointer; */
 		color: var(--purple-200);
 		stroke-width: 10;
 		transition: color 0.1s;
@@ -164,9 +215,24 @@ const IconContainer = styled.li`
 
 const RouteContainer = styled.div`
 	button {
+		cursor: pointer;
 		outline: none;
-		background-color: none;
+		border: 1px solid;
+		background: none;
+		width: 85px;
+		margin: 2px;
+		a {
+			width: 100%;
+		}
 	}
+`;
+
+const Member = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 30px 0;
+	font-size: 16px;
 `;
 
 export default RightNav;
