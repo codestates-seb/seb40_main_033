769ac5ad48e3a9525984ff415a1221d3522e9c30
uxolrv <uxolrv@gmail.com>
2022-11-22 18:01:46 +0900

    :sparkles: feat: 페이지 파일 생성 #66

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,6 +1,14 @@
 // 마이페이지
+// 여기로오면 mypage/user로 보내세요
+import { Outlet } from 'react-router-dom';
+
 function MyPage() {
-	return <h1>MyPage</h1>;
+	return (
+		<>
+			<h1>MyPage/</h1>
+			<Outlet />
+		</>
+	);
 }
 
 export default MyPage;
