71aedb3fa9fa4c0984ecaaef6838916d742201ec
uxolrv <uxolrv@gmail.com>
2022-11-22 18:05:35 +0900

    :lipstick: design: 레이아웃 구조 설계 #67

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { Outlet, useLocation } from 'react-router-dom';
+import LeftNav from './LeftNav';
+import RightNav from './RightNav';
+import Footer from './Footer';
+
+const hideURL = ['/login', '/signup'];
+
+function Layout() {
+	const { pathname } = useLocation();
+	const hide = hideURL.includes(pathname);
+	return (
+		<Container>
+			<TopContainer>
+				<LeftNav />
+				<MainContainer>
+					<Outlet />
+				</MainContainer>
+				<RightNav />
+			</TopContainer>
+			<Footer />
+		</Container>
+	);
+}
+
+export default Layout;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 100%;
+	min-height: 100vh;
+`;
+
+const TopContainer = styled.div`
+	display: flex;
+	position: relative;
+	width: 100%;
+	height: 100%;
+	justify-content: space-between;
+`;
+
+const MainContainer = styled.div`
+	display: flex;
+	max-width: 1240px;
+`;
