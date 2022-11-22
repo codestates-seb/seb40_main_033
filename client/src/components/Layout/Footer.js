8564960842276cd0d098028a053947564eb1377e
uxolrv <uxolrv@gmail.com>
2022-11-22 17:24:21 +0900

    :lipstick: design: Footer 컴포넌트 생성 #19

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function Footer() {
+	return <FooterContainer>푸터욤</FooterContainer>;
+}
+
+const FooterContainer = styled.footer`
+	background-color: var(--gray-500);
+	width: 100%;
+	height: 365px;
+`;
+
+export default Footer;
