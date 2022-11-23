d381d24ed83f71c0e2c75dd6c5e6bec269d306a5
uxolrv <uxolrv@gmail.com>
2022-11-23 11:06:55 +0900

    :lipstick: design: 마이페이지 헤더 구현 및 적용 #75

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,16 +1,25 @@
 // 마이페이지
 // 여기로오면 mypage/user로 보내세요
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 import MypageTab from '../components/Tabs/MypageTab';
+import MyPageHeader from '../components/Etc/MyPageHeader';
 
 function MyPage() {
 	return (
-		<>
+		<Container>
+			<MyPageHeader />
 			<MypageTab />
-			<h1>MyPage/</h1>
 			<Outlet />
-		</>
+		</Container>
 	);
 }
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+`;
 
 export default MyPage;
