1ab2cc04640b60ecca42f5b6d770735d0e8bd310
uxolrv <uxolrv@gmail.com>
2022-11-27 17:07:39 +0900

    :sparkles: feat: 주문내역 페이지에서만 ToggleTab 렌더링되도록 구현 #118

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,15 +1,20 @@
 // 마이페이지
 // 여기로오면 mypage/user로 보내세요
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import MypageTab from '../components/Tabs/MypageTab';
 import MyPageHeader from '../components/Etc/MyPageHeader';
+import { ToggleTab } from '../components/Tabs/TabButtons';
 
 function MyPage() {
+	const { pathname } = useLocation();
+	// 일반/정기 토글이 주문내역에서만 보이도록
+	const show = pathname.includes('/order');
 	return (
 		<Container>
 			<MyPageHeader />
 			<MypageTab />
+			{show && <ToggleTab />}
 			<Outlet />
 		</Container>
 	);
