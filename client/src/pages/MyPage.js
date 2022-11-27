ed815130d38a306efe11996000e6b3e100fac5aa
uxolrv <uxolrv@gmail.com>
2022-11-28 02:14:24 +0900

    :sparkles: feat: "/mypage"로 접근 시 "/mypage/user-info"로 이동 구현 #121

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,7 +1,6 @@
-// 마이페이지
-// 여기로오면 mypage/user로 보내세요
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useEffect } from 'react';
 import MypageTab from '../components/Tabs/MypageTab';
 import MyPageHeader from '../components/Etc/MyPageHeader';
 import { ToggleTab } from '../components/Tabs/TabButtons';
@@ -10,11 +9,19 @@ const togglePath = ['/mypage/order/normal', '/mypage/order/subscription'];
 const leftValueArr = [0, 68];
 
 function MyPage() {
+	const navigate = useNavigate();
 	const { pathname } = useLocation();
 	const currentIdx = togglePath.indexOf(pathname);
 	const highlightLeftValue = leftValueArr[currentIdx];
+
 	// 일반/정기 토글이 주문내역에서만 보이도록
 	const show = pathname.includes('/order');
+
+	useEffect(() => {
+		if (pathname === '/mypage') {
+			navigate('/mypage/user-info');
+		}
+	}, []);
 
 	return (
 		<Container>
