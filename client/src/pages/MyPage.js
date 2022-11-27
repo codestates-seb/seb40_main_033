6c443679b1f1db9428a7e6a8695dd1a0d237a2c8
uxolrv <uxolrv@gmail.com>
2022-11-28 03:10:05 +0900

    :bug: fix: 주문내역 상세페이지에서 일반/정기 토글 렌더링되지 않도록 수정 #117

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,4 +1,10 @@
-import { Outlet, useLocation, useMatch, useNavigate } from 'react-router-dom';
+import {
+	Outlet,
+	useLocation,
+	useMatch,
+	useNavigate,
+	useParams,
+} from 'react-router-dom';
 import styled from 'styled-components';
 import { useEffect } from 'react';
 import MypageTab from '../components/Tabs/MypageTab';
@@ -9,17 +15,19 @@ const togglePath = ['/mypage/order/normal', '/mypage/order/subscription'];
 const leftValueArr = [0, 68];
 
 function MyPage() {
-	const isMatch = useMatch('/mypage');
-	const navigate = useNavigate();
+	const { id } = useParams();
 	const { pathname } = useLocation();
+	const isMypage = useMatch('/mypage');
+	const navigate = useNavigate();
+
 	const currentIdx = togglePath.indexOf(pathname); // toggleTab 초기값
 	const highlightLeftValue = leftValueArr[currentIdx]; // toggleTab 초기값
 
-	// 일반/정기 토글이 주문내역에서만 보이도록
-	const show = pathname.includes('/order');
+	// 일반/정기 토글이 주문내역 페이지에서만 보이도록
+	const show = pathname.includes('/order') && !id;
 
 	useEffect(() => {
-		if (isMatch) {
+		if (isMypage) {
 			navigate('/mypage/user-info');
 		}
 	}, []);
