2699f0ef1ca47cd98856b532f65aa7f2657797c4
uxolrv <uxolrv@gmail.com>
2022-11-28 00:21:34 +0900

    :lipstick: design: 가격필터 버튼에 "할인상품 모아보기" 추가 및 애니메이션 보완 #120

--- client/src/components/Buttons/SearchButtons/SortButton.js
+++ client/src/components/Buttons/SearchButtons/SortButton.js
@@ -5,6 +5,7 @@ import { BsToggles } from 'react-icons/bs';
 import { FiFilter } from 'react-icons/fi';
 import PriceButton from './PriceButton';
 import { LetterButtonColor } from '../LetterButton';
+import { LightPurpleButton } from '../PurpleButton';
 
 export function SortButton({ children }) {
 	const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +20,13 @@ export function SortButton({ children }) {
 				<MenuBox isOpen={isOpen}>
 					{menus.map((menu, idx) => (
 						<MenuLi key={`${idx.toString()}-${menu}`}>
-							<LetterButtonColor color="gray" colorCode="200" fontSize="14px">
+							<LetterButtonColor
+								color="gray"
+								colorCode="300"
+								hoverColor="gray"
+								hoverColorCode="400"
+								fontSize="14px"
+							>
 								{menu}
 							</LetterButtonColor>
 						</MenuLi>
@@ -47,28 +54,34 @@ export function PriceSortButton({ children }) {
 		// console.log(isOpen);
 	};
 	return (
-		<ButtonContainer isOpen={isOpen} price>
+		<ButtonContainer className="price-container" isOpen={isOpen} price>
 			{isOpen && <PriceButton min={0} max={100000} />}
 			<MainBox>
 				<OpenButton onClick={handleOpenClick} isOpen={isOpen}>
 					<IoIosArrowBack />
 				</OpenButton>
-				<NameBox>
-					{children}
-					<FiFilter />
-				</NameBox>
+				<RightContainer isOpen={isOpen}>
+					<NameBox>
+						{children}
+						<FiFilter />
+					</NameBox>
+					{isOpen && (
+						<LightPurpleButton fontSize="11px" width="70px" fontWeight="bold">
+							할인상품
+							<br />
+							모아보기
+						</LightPurpleButton>
+					)}
+				</RightContainer>
 			</MainBox>
 		</ButtonContainer>
 	);
 }
 
 const ButtonContainer = styled.div`
-	/* margin-left: 50%; */
 	position: relative;
 	display: inline-flex;
-	justify-content: space-between;
 	align-items: center;
-	align-items: baseline;
 	box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.07);
 	background-color: white;
 	border-radius: 27px;
@@ -80,13 +93,16 @@ const ButtonContainer = styled.div`
 	${(props) =>
 		props.price && props.isOpen
 			? css`
-					align-items: baseline;
 					height: auto;
-					padding: 13px 20px 13px 20px;
 			  `
-			: null}/* position: absolute;
-	top: 0;
-	right: 0; //float: right 대신 써줘도 됩니다. 둘다 보기에는 같은데 나중에 뭐 쓸지 함 봐야할듯.*/
+			: null}
+	&.price-container {
+		align-items: flex-start;
+		position: absolute;
+		right: 0px;
+		top: -60px;
+		z-index: 999;
+	}
 `;
 
 const OpenButton = styled.button`
@@ -108,8 +124,9 @@ const OpenButton = styled.button`
 			color: var(--purple-200);
 		}
 	}
-	transform: ${({ isOpen }) => (isOpen ? 'scaleX(-1)' : null)};
+	transform: ${({ isOpen }) => (isOpen ? 'rotate(540deg)' : null)};
 `;
+
 const MainBox = styled.div`
 	background-color: white;
 	z-index: 1;
@@ -120,12 +137,24 @@ const MainBox = styled.div`
 	align-items: center;
 `;
 
+const RightContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	button {
+		bottom: 20.5px;
+		right: 32px;
+		position: absolute;
+		animation: slide-fade-in-dropdown-animation 0.3s ease-in-out;
+	}
+`;
+
 const NameBox = styled.div`
 	z-index: 1;
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
-	width: 72px;
+	width: 74px;
+	font-size: 14px;
 	color: var(--purple-200);
 	font-weight: var(--bold);
 	& > svg {
@@ -153,22 +182,27 @@ const MenuBox = styled.ul`
 	@keyframes slide-fade-in-dropdown-animation {
 		0% {
 			transform: translateX(40%);
+			opacity: 0%;
 		}
 
 		100% {
 			transform: translateX(0);
+			opacity: 100%;
 		}
 	}
 	@keyframes slide-fade-out-dropdown-animation {
 		0% {
 			transform: translateX(0);
+			opacity: 0%;
 		}
 
 		100% {
 			transform: translateX(-100%);
+			opacity: 100%;
 		}
 	}
 `;
+
 const MenuLi = styled.li`
 	float: left;
 	color: var(--gray-300);
