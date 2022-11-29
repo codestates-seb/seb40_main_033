121de680db42e37828363a3235440c4a010d98fe
dohyeons <ddhhss0603@gmail.com>
2022-11-29 23:35:11 +0900

    :lipstick: design: 정기구독관리 목업 구현 #136

--- client/src/pages/MyPages/SubManage.js
+++ client/src/pages/MyPages/SubManage.js
@@ -1,8 +1,23 @@
 // 정기구독관리
+import styled, { css } from 'styled-components';
 import SubManagementList from '../../components/Lists/MyPageLists/SubManagementList';
 
 function SubManage() {
-	return <SubManagementList />;
+	return (
+		<SubManageContainer>
+			<SubManagementList />
+			<SubManagementList />
+			<SubManagementList />
+			<SubManagementList />
+			<SubManagementList />
+			<SubManagementList />
+		</SubManageContainer>
+	);
 }
 
 export default SubManage;
+
+const SubManageContainer = styled.main`
+	display: flex;
+	flex-direction: column;
+`;
