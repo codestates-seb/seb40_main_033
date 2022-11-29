0a328b216aac2efabd5439c5049fca089b087d82
uxolrv <uxolrv@gmail.com>
2022-11-29 21:57:33 +0900

    :lipstick: design: Talk 작성 폼 props에 placeholder 추가 #137

--- client/src/components/Forms/TalkForm.js
+++ client/src/components/Forms/TalkForm.js
@@ -1,10 +1,10 @@
 // 상세페이지의 talk 작성 form
 import DefalutForm from './DefalutForm';
 
-function TalkForm() {
+function TalkForm({ placeholder }) {
 	return (
 		<DefalutForm
-			placeholder="보고 있는 상품에 대한 글을 남겨주세요."
+			placeholder={placeholder || '보고 있는 상품에 대한 글을 남겨주세요.'}
 			maxLength={150}
 			height={130}
 			target="토크"
