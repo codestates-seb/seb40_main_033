111b374e8b3bbeeaa35885275ca737f3cebed04d
dohyeons <ddhhss0603@gmail.com>
2022-12-05 15:35:57 +0900

    :art: style: 주석 삭제 #192

--- client/src/components/Buttons/WishlistButton.js
+++ client/src/components/Buttons/WishlistButton.js
@@ -1,24 +1,13 @@
 import styled from 'styled-components';
 import { FaHeart } from 'react-icons/fa';
-import { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useState } from 'react';
 import { toast } from 'react-toastify';
 import { usePost } from '../../hooks/useFetch';
 
 function WishlistButton({ isChecked, itemId, setIsChecked }) {
 	const [request, setRequest] = useState(isChecked ? 0 : 1);
 	const token = localStorage.getItem('accessToken');
-	// const navigate = useNavigate();
-
-	// useEffect(() => {
-	// 	if (isChecked === 1) {
-	// 		setRequest(0);
-	// 	} else {
-	// 		setRequest(1);
-	// 	}
-	// }, [isChecked]);
-
-	const { mutate, response } = usePost(
+	const { mutate } = usePost(
 		`http://ec2-43-201-37-71.ap-northeast-2.compute.amazonaws.com:8080/wishes/${itemId}?wish=${request}`,
 	);
 
