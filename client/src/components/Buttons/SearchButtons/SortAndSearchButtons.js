d10f3d77291e8db446d81e1d647b48eb2893bdd7
kihyeoon <bangion93@gmail.com>
2022-12-06 11:23:16 +0900

    🎨 style: 미사용 refetch 제거 #192

--- client/src/components/Buttons/SearchButtons/SortAndSearchButtons.js
+++ client/src/components/Buttons/SearchButtons/SortAndSearchButtons.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 import { PriceSortButton, SortButton } from './SortButton';
 
-export default function SortAndSearchButtons({ refetch }) {
+export default function SortAndSearchButtons() {
 	return (
 		<ButtonsBox>
-			<PriceSortButton refetch={refetch}>가격 필터</PriceSortButton>
-			<SortButton refetch={refetch}>정렬 방법</SortButton>
+			<PriceSortButton>가격 필터</PriceSortButton>
+			<SortButton>정렬 방법</SortButton>
 		</ButtonsBox>
 	);
 }
