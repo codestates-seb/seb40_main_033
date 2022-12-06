25241b219791b8a8306ab573d2dcb0b403bb259e
dohyeons <ddhhss0603@gmail.com>
2022-12-07 01:26:59 +0900

    :art: style: 주석 삭제 및 useCallback #192

--- client/src/components/Lists/MyPageLists/OrderList.js
+++ client/src/components/Lists/MyPageLists/OrderList.js
@@ -3,7 +3,6 @@ import { IoIosArrowForward, IoMdClose } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 import { useCallback, useState } from 'react';
 import { toast } from 'react-toastify';
-import { BsListStars } from 'react-icons/bs';
 import { LetterButtonColor } from '../../Buttons/LetterButton';
 import { DotDate } from '../../Etc/ListDate';
 import Price from '../../Etc/Price';
@@ -13,7 +12,7 @@ import { useDelete } from '../../../hooks/useFetch';
 function OrderList({ list }) {
 	const navigate = useNavigate();
 	const [openCancel, setOpenCancel] = useState(false);
-	const { mutate, isLoading, isError, error, response } = useDelete(
+	const { mutate } = useDelete(
 		`http://ec2-43-201-37-71.ap-northeast-2.compute.amazonaws.com:8080/orders/${list.orderId}`,
 	);
 
@@ -25,10 +24,6 @@ function OrderList({ list }) {
 	const handleDetailClick = useCallback(() => {
 		navigate(`/mypage/order/${list.orderId}`);
 	}, []);
-
-	const handleItemClick = () => {
-		navigate(`/detail/${list.item.itemId}`);
-	};
 
 	// 취소 모달
 	const handleCancelClick = useCallback(() => {
@@ -50,7 +45,7 @@ function OrderList({ list }) {
 			<Image
 				src={list.item.thumbnail}
 				alt="상품 이미지"
-				onClick={handleItemClick}
+				onClick={handlePageMove}
 			/>
 			<MainContainer>
 				<InfoContainer>
@@ -58,7 +53,7 @@ function OrderList({ list }) {
 						<DeliveryStatus>{status}</DeliveryStatus>
 						<DotDate date={list.createdAt} />
 					</ShoppingInfo>
-					<Name onClick={handleItemClick}>
+					<Name onClick={handlePageMove}>
 						{`${list.item.brand}, ${list.item.title}, ${list.item.capacity}정 
 						${totalNum > 1 ? `외 ${totalNum - 1}개` : ''}`}
 					</Name>
