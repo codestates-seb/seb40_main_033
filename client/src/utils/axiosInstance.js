8bbd65d232885ef99ff44175ca74e9a027e14f64
kihyeoon <bangion93@gmail.com>
2022-12-06 23:13:55 +0900

    ♻️ refator: interceptors 연결 #234

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -4,12 +4,12 @@ import store from '../redux/store/store';
 
 const axiosInstance = axios.create({
 	baseURL: 'http://ec2-43-201-37-71.ap-northeast-2.compute.amazonaws.com:8080', // 서버 url
-	timeout: 5000,
+	timeout: 3000,
 });
 
 export default axiosInstance;
 
-axios.interceptors.request.use((config) => {
+axiosInstance.interceptors.request.use((config) => {
 	const { accessToken } = store.getState().user;
 	config.headers.Authorization = accessToken;
 
