ddc76e2262766f2e305aaa96532fcfd2c94fc406
kihyeoon <bangion93@gmail.com>
2023-03-18 19:38:27 +0900

    ♻️ refactor: CartList 컴포넌트 타입 적용 #277
    cart.type 일부 변경

--- client/src/types/cart.type.ts
+++ client/src/types/cart.type.ts
@@ -13,20 +13,30 @@ export interface CartData {
 }
 
 export interface CartItems {
-	data: {
-		itemCartId: number;
-		quantity: number;
-		period: number;
-		buyNow: boolean;
-		subscription: boolean;
-		item: CartItem;
-		createdAt: Date;
-		updatedAt: Date;
-	}[];
+	data: CartItemWithData[];
 	pageInfo: null;
+}
+
+export interface CartItemWithData {
+	itemCartId: number;
+	quantity: number;
+	period: number;
+	buyNow: boolean;
+	subscription: boolean;
+	item: CartItem;
+	createdAt: Date;
+	updatedAt: Date;
 }
 
 export type CartItem = Omit<
 	Item,
-	'content' | 'starAvg' | 'reviewSize' | 'nutritionFacts'
->;
+	'content' | 'starAvg' | 'reviewSize' | 'nutritionFacts' | 'discountPrice'
+> & {
+	disCountPrice: number;
+};
+
+export interface CartListProps {
+	data: CartItemWithData;
+	item: CartItem;
+	sub?: boolean;
+}
