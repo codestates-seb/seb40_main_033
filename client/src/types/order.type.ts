3ca3fed7c10f21f542c3003152f06813eb43bb1d
uxolrv <uxolrv@gmail.com>
2023-04-06 00:07:38 +0900

    :recycle: refactor: 주문내역 상세조회 페이지 타입스크립트 적용 #293

--- client/src/types/order.type.ts
+++ client/src/types/order.type.ts
@@ -1,4 +1,5 @@
 import { ItemShortcutData } from './item.type';
+import { PayData } from './payment.type';
 
 export interface UseGetOrderListProps {
 	pathname: string;
@@ -19,4 +20,25 @@ export interface OrderListData {
 	subscription: boolean;
 	totalItems: number;
 	updatedAt: Date;
+}
+
+export interface OrderDetailListProps {
+	inModal?: boolean;
+	itemOrderId: number;
+	itemId: number;
+	brand: string;
+	thumbnail: string;
+	title: string;
+	quantity: number;
+	nowPrice: number;
+	discountRate: number;
+	beforePrice: number;
+	period: number;
+	subscription: boolean;
+	capacity: number;
+	orderStatus: string;
+}
+
+export interface OrderDetailData {
+	data: PayData;
 }
