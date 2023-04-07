104084026f84051c5c8639dc6fbbebd772b34e77
uxolrv <uxolrv@gmail.com>
2023-04-07 23:26:39 +0900

    :lipstick: design: 모달 styled-components로 스타일링 #302

--- client/src/components/Modals/DefalutModal.js
+++ client/src/components/Modals/DefalutModal.js
@@ -1,35 +1,83 @@
-import { useCallback } from 'react';
+import { useState } from 'react';
 import Modal from 'react-modal';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { TempLogo } from '../../assets/Icons';
 import { PurpleButton, LightPurpleButton } from '../Buttons/PurpleButton';
-import './style.css';
 
-const customStyles = {
-	overlay: {
-		backgroundColor: 'rgba(172, 174, 187, 0.2)',
-	},
-	content: {
-		animation: 'modalFadeIn 0.3s ease-in-out',
-		minWidth: '500px',
-		heigth: '384px',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
-		top: '50%',
-		left: '50%',
-		right: 'auto',
-		bottom: 'auto',
-		transform: 'translate(-50%, -50%)',
-		background: '#ffffff',
-		padding: '60px 75px',
-		border: 'none',
-		borderRadius: '10px',
-		boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.05)',
-	},
-};
+const StyledModal = styled(ReactModalAdapter)`
+	@keyframes modalFadeIn {
+		from {
+			transform: translate(-50%, -60%);
+			opacity: 0;
+		}
+		to {
+			transform: translate(-50%, -50%);
+			opacity: 1;
+		}
+	}
+
+	@keyframes modalFadeOut {
+		from {
+			transform: translate(-50%, -50%);
+			opacity: 0.8;
+		}
+		to {
+			transform: translate(-50%, -60%);
+			opacity: 0;
+		}
+	}
+
+	&__content {
+		min-width: 500px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		top: 50%;
+		left: 50%;
+		right: auto;
+		bottom: auto;
+		position: fixed;
+		transform: translate(-50%, -50%);
+		background: #ffffff;
+		padding: 60px 75px;
+		border: none;
+		border-radius: 10px;
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05);
+
+		${({ isDelay }) =>
+			!isDelay // 딜레이가 아님 === 열림
+				? css`
+						animation: modalFadeIn 0.3s ease;
+				  `
+				: css`
+						animation: modalFadeOut 0.35s ease;
+				  `};
+	}
+
+	&__overlay {
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		right: 0px;
+		bottom: 0px;
+		background-color: rgba(172, 174, 187, 0.2);
+	}
+`;
+
+function ReactModalAdapter({ className, ...props }) {
+	const contentClassName = `${className}__content`;
+	const overlayClassName = `${className}__overlay`;
+	return (
+		<Modal
+			portalClassName="modal"
+			className={contentClassName}
+			overlayClassName={overlayClassName}
+			{...props}
+		/>
+	);
+}
 
 // 모달에게 숨겨질 부분
 Modal.setAppElement('#root');
@@ -49,15 +97,23 @@ function DefalutModal({
 	onClickPbtn, // 버튼에 넣고 싶은 onClick 함수
 	onClickLpbtn,
 	autoClose,
+	children,
 }) {
 	const navigate = useNavigate();
+	const [isDelay, setIsDelay] = useState(false);
 
-	const closeModal = useCallback(() => {
-		setIsOpen(false);
+	const closeModal = () => {
+		setIsDelay(true);
+
+		setTimeout(() => {
+			setIsOpen(false);
+			setIsDelay(false);
+		}, 300);
+
 		if (path) {
 			navigate(path);
 		}
-	}, []);
+	};
 
 	// 모달 열었을 때 작동하는 함수 (필요 시 사용)
 	const afterOpenModal = () => {
@@ -69,43 +125,48 @@ function DefalutModal({
 	};
 
 	return (
-		<Modal
+		<StyledModal
 			isOpen={modalIsOpen}
-			closeTimeoutMS={500}
+			isDelay={isDelay}
 			onAfterOpen={afterOpenModal}
 			onRequestClose={closeModal}
-			style={customStyles}
-			portalClassName="modal"
 		>
-			<Icon>
-				<TempLogo />
-			</Icon>
-			<Title>{title}</Title>
-			<FormContainer>
-				{list}
-				{star}
-				{form}
-			</FormContainer>
-			{contents ? <Contents>{contents}</Contents> : null}
-			{subContents ? <SubContents>{subContents}</SubContents> : null}
-			{lpbtnTexts || pbtnTexts ? (
-				<BtnContainer>
-					{lpbtnTexts ? (
-						<LightPurpleButton
-							fontSize="13px"
-							onClick={onClickLpbtn || closeModal}
-						>
-							{lpbtnTexts}
-						</LightPurpleButton>
+			{children || (
+				<>
+					<Icon>
+						<TempLogo />
+					</Icon>
+					<Title>{title}</Title>
+					<FormContainer>
+						{list}
+						{star}
+						{form}
+					</FormContainer>
+					{contents ? <Contents>{contents}</Contents> : null}
+					{subContents ? <SubContents>{subContents}</SubContents> : null}
+					{lpbtnTexts || pbtnTexts ? (
+						<BtnContainer>
+							{lpbtnTexts ? (
+								<LightPurpleButton
+									fontSize="13px"
+									onClick={onClickLpbtn || closeModal}
+								>
+									{lpbtnTexts}
+								</LightPurpleButton>
+							) : null}
+							{pbtnTexts ? (
+								<PurpleButton
+									fontSize="13px"
+									onClick={onClickPbtn || closeModal}
+								>
+									{pbtnTexts}
+								</PurpleButton>
+							) : null}
+						</BtnContainer>
 					) : null}
-					{pbtnTexts ? (
-						<PurpleButton fontSize="13px" onClick={onClickPbtn || closeModal}>
-							{pbtnTexts}
-						</PurpleButton>
-					) : null}
-				</BtnContainer>
-			) : null}
-		</Modal>
+				</>
+			)}
+		</StyledModal>
 	);
 }
 
