bf8fedc91ef49fa113a53aed325adff7a3d46206
kihyeoon <bangion93@gmail.com>
2023-04-16 03:48:29 +0900

    💚 chore: craco.config 파일 lint 적용 #303

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const CracoEsbuildPlugin = require('craco-esbuild');
 const webpack = require('webpack');
 
